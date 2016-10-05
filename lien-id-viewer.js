5b77afe2d47313a47e6a57eaefe343193e838682
Mark D'Arensbourg <mdarens@gmail.com>
2016-10-05 08:56:49 -0500

    npmify and eslintify (#1)
    * npmify and eslintify
    * v2.0.1

--- lien-id-viewer.js
+++ lien-id-viewer.js
@@ -1,43 +1,27 @@
-﻿(function (root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(["knockout", "jquery", "knockout-place-viewer"], factory);
-	} else {
-		// Browser globals
-		factory(ko, $);
-	}
-}(this, function (ko, $) {
-	ko.bindingHandlers.lienIdViewer = {
-		init: function (el, valueAccessor, allBindings) {
-			return { controlsDescendantBindings: true };
-		},
-		update: function (el, valueAccessor, allBindings) {
-			var lien = ko.utils.unwrapObservable(valueAccessor());
-			var addressTemplate = ko.unwrap(allBindings().addressTemplate);
+﻿var ko = require("knockout");
 
-			if (!lien) {
-				el.innerHTML = "No lien provided";
-			}
+ko.bindingHandlers.lienIdViewer = {
+	init: function () {
+		return { controlsDescendantBindings: true };
+	},
+	update: function (el, valueAccessor, allBindings) {
+		var lien = ko.utils.unwrapObservable(valueAccessor());
+		var addressTemplate = ko.unwrap(allBindings().addressTemplate);
 
-			else {
-				if (!lien.address.isEmpty()) {
-					if (addressTemplate) {
-						ko.renderTemplate(addressTemplate, lien.address, null, el, "replaceChildren");
-					}
-
-					else {
-						el.innerHTML = lien.address.formatted();
-					}
+		if (!lien) {
+			el.innerHTML = "No lien provided";
+		} else {
+			if (!lien.address.isEmpty()) {
+				if (addressTemplate) {
+					ko.renderTemplate(addressTemplate, lien.address, null, el, "replaceChildren");
+				} else {
+					el.innerHTML = lien.address.formatted();
 				}
-
-				else if (lien.filingLocationPlace()) {
-					ko.bindingHandlers.placeViewer.update(el, lien.filingLocationPlace);
-				}
-
-				else {
-					el.innerHTML = "No lien identifier";
-				}
+			} else if (lien.filingLocationPlace()) {
+				ko.bindingHandlers.placeViewer.update(el, lien.filingLocationPlace);
+			} else {
+				el.innerHTML = "No lien identifier";
 			}
 		}
-	};
-}));
+	}
+};
